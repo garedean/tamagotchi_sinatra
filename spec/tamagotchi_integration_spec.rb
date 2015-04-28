232803386d9daebb5f8e1843318c00756c973de1
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 15:08:59 -0700

    Test the Feed button in the tamagotchi path

--- spec/tamagotchi_integration_spec.rb
+++ spec/tamagotchi_integration_spec.rb
@@ -1,1 +1,12 @@
-require('capybara')
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('the tamagotchi path', :type => :feature) do
+  it('responds to being fed') do
+    visit('/')
+    click_button('Feed')
+    expect(page).to(have_content('Your Tamagotchi has 20 food in it'))
+  end
+
+end

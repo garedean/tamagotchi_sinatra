a5d307ee57060de196ac7ece89bfaf11cac734e9
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 16:17:38 -0700

    Add logic feeding and displaying food level

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ require('./lib/tamagotchi')
 also_reload('./lib/*.rb')
 
 get('/') do
-  @food_amount = params.fetch('food-amount')
+  if params['food-amount']
+    food_amount = params.fetch('food-amount').to_i
+  else
+    food_amount = 10
+  end
+
+  critter = Tamagotchi.new('bob')
+  critter.feed(food_amount)
+  @food_amount = critter.food_level
   erb(:index)
 end

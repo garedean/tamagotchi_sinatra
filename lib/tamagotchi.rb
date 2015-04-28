ee59f900bb14c2f47a61c1b9837bb1205225b57e
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 14:11:29 -0700

    Change methods for getting and setting name to attribute accessors; add food_level attribute accessor

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,13 +1,9 @@
 class Tamagotchi
+  attr_accessor :food_level
+  attr_accessor :name
+
   define_method(:initialize) do |name|
     @name = name
-  end
-
-  define_method(:name) do
-    @name
-  end
-
-  define_method(:set_name) do |new_name|
-    @name = new_name
+    @food_level = 10
   end
 end

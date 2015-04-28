32b2989119ac53a0c6f7776f71deef48f0eabb2a
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 15:43:05 -0700

    Add @food_amount variable for 'food-amount' param value

--- app.rb
+++ app.rb
@@ -1,1 +1,9 @@
 require('sinatra')
+require('sinatra/reloader')
+require('./lib/tamagotchi')
+also_reload('./lib/*.rb')
+
+get('/') do
+  @food_amount = params.fetch('food-amount')
+  erb(:index)
+end

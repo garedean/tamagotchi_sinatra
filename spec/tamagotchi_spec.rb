f2867f5e9489b2bd70796878a83305397e5837d9
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 13:54:29 -0700

    Test that #set_name sets a name

--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -7,4 +7,10 @@ describe('Tamagotchi') do
       expect(Tamagotchi.new('Bob').name).to(eq('Bob'))
     end
   end
+
+  describe('#set_name') do
+    it('sets a name for the tamagotchi') do
+      expect(Tamagotchi.new('Bob').set_name('Bill')).to(eq('Bill'))
+    end
+  end
 end
